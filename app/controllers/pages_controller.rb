a1d3bd3d5f06b0701fcabd217a176d135cf370ef
Hoan Nguyen <nguyenhoan2510@gmail.com>
2017-12-06 23:43:24 +0700

    Get twitter's tweets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :client
 
   def index
+    @tweets = @client.user_timeline('twitter', count: 200)
+  end
+
+  private
+
+  def client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.twitter_app_id
+      config.consumer_secret     = Rails.application.secrets.twitter_app_secret
+      config.access_token        = Rails.application.secrets.twitter_access_token
+      config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+    end
   end
 end
